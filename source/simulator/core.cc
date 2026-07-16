c17c475dc3d43c6a1d688c9218782ad3c27957af
tiannh7 <tiannh77@gmail.com>
2026-07-16 15:51:31 +0800

    Persist advection residual temporary

--- source/simulator/core.cc
+++ source/simulator/core.cc
@@ -1594,6 +1594,7 @@ namespace aspect
     old_old_solution.reinit(introspection.index_sets.system_partitioning, introspection.index_sets.system_relevant_partitioning, mpi_communicator);
     current_linearization_point.reinit (introspection.index_sets.system_partitioning, introspection.index_sets.system_relevant_partitioning, mpi_communicator);
     advection_distributed_solution.reinit(introspection.index_sets.system_partitioning, mpi_communicator);
+    advection_distributed_residual.reinit(introspection.index_sets.system_partitioning, mpi_communicator);
 
     if (parameters.use_operator_splitting)
       {
